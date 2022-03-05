74b97aa75f5b11fa17d81b68b559da2ce11e69a8
Hrishikesh Kalita <hrishikeshkalita11@gmail.com>
2022-03-05 17:38:18 +0530

    Updates

---
+++ precache-manifest.79bbe73964dc3334bc23046e56d76c9e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67497ccb4a1ef49833ce776d42228381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e57eecd4a9e10109891",
+    "url": "/static/css/main.cc41272b.chunk.css"
+  },
+  {
+    "revision": "7f894b6a3b88603082a8",
+    "url": "/static/js/2.d6459c00.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.d6459c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e57eecd4a9e10109891",
+    "url": "/static/js/main.88481b31.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "7f85db890c6dc15e7f7be8155abb9f22",
+    "url": "/static/media/hrishikesh.7f85db89.png"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/static/media/psykinetic.9c1952ca.png"
+  }
+]);
