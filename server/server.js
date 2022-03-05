e4b918dbcd0309a60056425d038cf109bdce2d52
Hrishikesh Kalita <hrishikeshkalita11@gmail.com>
2022-03-05 17:52:29 +0530

    Repo homepage added

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const http = require('http')
-const app = express()
-app.use(express.static('build'))
+// const express = require('express')
+// const http = require('http')
+// const app = express()
+// app.use(express.static('build'))
 
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 
-const server = http.createServer(app)
+// const server = http.createServer(app)
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'This page does not exist!' })
-}
+// const unknownEndpoint = (request, response) => {
+//   response.status(404).send({ error: 'This page does not exist!' })
+// }
 
-app.use(unknownEndpoint)
+// app.use(unknownEndpoint)
 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+// server.listen(port, () => {
+//   console.log(`Server running on port ${port}`)
+// })
